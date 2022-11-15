946bf55c4576b85df9c45a882a432e5461e152c3
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2022-11-15 11:10:49 -0300

    DiceResult component creation

---
+++ components/DiceResult.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+
+const faces = [
+	require('../assets/dice1.png'),
+	require('../assets/dice2.png'),
+	require('../assets/dice3.png'),
+	require('../assets/dice4.png'),
+	require('../assets/dice5.png'),
+	require('../assets/dice6.png'),
+]
+
+export default function DiceResult(props) {
+	return (
+		<View style={{ backgroundColor: props.backgroundColor, borderRadius: 10, alignSelf: 'baseline' }}>
+			<Image
+				style={{ width: props.width, height: props.height, tintColor: props.tintColor }}
+				source={faces[(props.faceNumber - 1) % 6]}
+			/>
+		</View>
+	);
+}
